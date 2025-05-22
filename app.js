bdf0d8da96b60962a99b3e2c8954eacc1dcf9bf0
ufdevs <ramesh_25s12res130@iitp.ac.in>
2025-05-22 22:56:51 +0530

    corssss

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('./config/db');
 const walletRouter = require('./routes/walletRoutes');
 const userRouter = require('./routes/userRoutes');
-const cors = require('cors');
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
