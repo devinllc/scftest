0b298b53afb81f350c3037f443324c522c1f2e53
Ramesh Vishwakarma <114978317+devinllc@users.noreply.github.com>
2025-05-22 18:21:19 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const debug = require("debug")("app:mongoose");
 
-// Load .env
+
 dotenv.config();
 // Load config from .env or fallback
 const mongoUri = `${process.env.MONGODB_URI || "mongodb://localhost:27017"}/${process.env.DB_NAME || "test"}`;
